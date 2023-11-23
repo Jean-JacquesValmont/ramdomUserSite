79298951d34ed254f72426507002667de7f9315a
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-11-23 18:17:34 +0100

    begin Tailwind css in card

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { FaUserAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaBirthdayCake } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const Card = (props) => {
 
   return (
-    <div>
+    <div className="flex flex-col text-center justify-center">
         <img src={props.item.picture.large} alt=""/>
+        <div className="flex justify-between">
+        <FaUserAlt />
+        <FaBirthdayCake />
+        <MdEmail />
+        <FaMapMarkerAlt />
+        <FaPhoneAlt />
+        <FaLock />
+        </div>
         <p>Name: {props.item.name.first} {props.item.name.last}</p>
         <p>Birthday: {props.item.registered.date.substring(0, 10)}</p>
         <p>Email: {props.item.email}</p>
