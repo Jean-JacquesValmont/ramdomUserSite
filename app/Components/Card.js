da1a99bd0a7be5dbeb6f6ed844327eb2b4233ee6
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-11-23 17:35:53 +0100

    Fetch two Users and display cards

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div>
-        <img src={props.item[0].picture.large}/>
-        <p>Name: {props.item[0].name.first} {props.item[0].name.last}</p>
-        <p>Birthday: {props.item[0].registered.date}</p>
-        <p>Email: {props.item[0].email}</p>
-        <p>Adress: {props.item[0].location.street.number} {props.item[0].location.street.name}</p>
-        <p>Phone: {props.item[0].cell}</p>
-        <p>Password: {props.item[0].login.password}</p>
+        <img src={props.item.picture.large}/>
+        <p>Name: {props.item.name.first} {props.item.name.last}</p>
+        <p>Birthday: {props.item.registered.date}</p>
+        <p>Email: {props.item.email}</p>
+        <p>Adress: {props.item.location.street.number} {props.item.location.street.name}</p>
+        <p>Phone: {props.item.cell}</p>
+        <p>Password: {props.item.login.password}</p>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -5,18 +5,26 @@ import useFetch from "./CustomHook/UseFetch"
 import Card from "./Components/Card"
 
 export default function Home() {
-  const dataRamdomUsers = useFetch("https://randomuser.me/api")
+  const dataRamdomUsers = useFetch("https://randomuser.me/api/?results=2")
 
   if (!dataRamdomUsers.results || dataRamdomUsers.results.length === 0) {
     // Les données ne sont pas encore disponibles
     return <div>Loading...</div>;
   }
 
+  const cardsUser = dataRamdomUsers.results.map(item => {
+    return (
+        <Card
+            key={dataRamdomUsers.results.indexOf(item)}
+            item={item}
+        />
+    )
+  })
+
   return (
     <main>
       <div>
-        <Card
-        item = {dataRamdomUsers.results}/>
+        {cardsUser}
       </div>
     </main>
   )
