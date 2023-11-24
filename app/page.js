0f814ed65e3d4bb1b98e162077e967335bde9cfe
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-11-24 10:53:43 +0100

    Upgrade button function and CSS

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useState } from "react"
 import useFetch from "./CustomHook/UseFetch"
 import Card from "./Components/Card"
@@ -7,20 +6,23 @@ import Card from "./Components/Card"
 export default function Home() {
   const [numberOfUser, setNumberOfUser] = useState(5)
   const dataRamdomUsers = useFetch("https://randomuser.me/api/?results="+String(numberOfUser), numberOfUser)
+  const styleButton = "bg-green-300 border-2 border-black rounded-lg p-2 hover:bg-green-600";
 
   if (!dataRamdomUsers.results || dataRamdomUsers.results.length === 0) {
     return <div>Loading...</div>;
   }
 
-  const increaseUser = () => {
-    setNumberOfUser(numberOfUser + 1)
+  const modifiedNumberOfUser = (number) => {
+    setNumberOfUser(numberOfUser + number)
+    if(numberOfUser <= 0)
+      {setNumberOfUser(2)}
     console.log(numberOfUser)
   }
 
-  const decreaseUser = () => {
-    setNumberOfUser(numberOfUser - 1)
-    console.log(numberOfUser)
-  }
+  const increasedNumberByOne = () => modifiedNumberOfUser(1)
+  const increasedNumberByTen = () => modifiedNumberOfUser(10)
+  const decreaseNumberByOne = () => modifiedNumberOfUser(-1)
+  const decreaseNumberByTen = () => modifiedNumberOfUser(-10)
 
   const cardsUser = dataRamdomUsers.results.map(item => {
     return (
@@ -33,9 +35,12 @@ export default function Home() {
 
   return (
     <main className="bg-white">
-      <button onClick={increaseUser}>Increase by 1 user</button>
-      <button onClick={increaseUser}>Increase by 10 user</button>
-      <button onClick={decreaseUser}>Decreased by 1 user</button>
+      <div className="flex justify-between m-4">
+        <button className={styleButton} onClick={increasedNumberByOne}>Increase by 1 user</button>
+        <button className={styleButton} onClick={increasedNumberByTen}>Increase by 10 user</button>
+        <button className={styleButton} onClick={decreaseNumberByOne}>Decreased by 1 user</button>
+        <button className={styleButton} onClick={decreaseNumberByTen}>Decreased by 10 user</button>
+      </div>
       <div className="m-4 grid grid-cols-5 gap-6">
         {cardsUser}
       </div>
